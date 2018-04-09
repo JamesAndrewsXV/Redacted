b271e24e5d37c90a0615e80dbd95fe0c60d0f86c
JamesAndrewsXV <jamesandrewsxv@gmail.com>
2018-04-09 19:51:51 -0400

    The Force Awakens

--- New Unity Project/Assets/Scripts/Text.cs
+++ New Unity Project/Assets/Scripts/Text.cs
@@ -10,7 +10,7 @@ public class Text : MonoBehaviour {
 
     public Vector2 minForce;
     public Vector2 maxForce;
-
+    public Vector2 mousePos;
     void Awake() {
         textCol = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
@@ -20,11 +20,14 @@ public class Text : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Debug.Log(mainCam.ScreenToWorldPoint(Input.mousePosition));
-            if (textCol.bounds.Contains(mainCam.ScreenToWorldPoint(Input.mousePosition))) {
+            float mousePosX = mainCam.ScreenToWorldPoint(Input.mousePosition).x;
+            float mousePosY = mainCam.ScreenToWorldPoint(Input.mousePosition).y;
+            mousePos = new Vector3(mousePosX, mousePosY, 0);// select distance = 10 units from the camera
+            Debug.Log(mainCam.ScreenToWorldPoint(mousePos));
+            if (textCol.bounds.Contains(mousePos)) {
                 Debug.Log("here");
                 rb.bodyType = RigidbodyType2D.Dynamic;
-                rb.AddForce(new Vector2(Random.Range(minForce.x, maxForce.x), Random.Range(minForce.y, maxForce.y)));
+                rb.AddForce(new Vector3(Random.Range(minForce.x, maxForce.x), Random.Range(minForce.y, maxForce.y), 0));
             }
         }
     }
