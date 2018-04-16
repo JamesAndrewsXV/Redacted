d81093c6e5a6deeea000a022cefab51f89a6e009
tday5 <trevday@icloud.com>
2018-04-16 14:33:42 -0400

    max three clicks and fix rotation

--- New Unity Project/Assets/Scripts/Text.cs
+++ New Unity Project/Assets/Scripts/Text.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Text : MonoBehaviour {
 
+	static int totalClicked = 0;
+
 	BoxCollider2D textCol;
 	Rigidbody2D rb;
 	Camera mainCam;
 	bool touched;
 	Vector2 originalPos;
+	Quaternion originalRot;
 
 	GameObject plane;
 	public GameObject redaction;
@@ -18,9 +21,11 @@ public class Text : MonoBehaviour {
 	public Vector2 maxForce;
 	public Vector2 mousePos;
 	void Awake() {
+		totalClicked = 0;
 		textCol = GetComponent<BoxCollider2D>();
 		rb = GetComponent<Rigidbody2D>();
 		originalPos = transform.position;
+		originalRot = transform.rotation;
 		touched = false;
 		mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 	}
@@ -31,7 +36,8 @@ public class Text : MonoBehaviour {
 			float mousePosX = mainCam.ScreenToWorldPoint(Input.mousePosition).x;
 			float mousePosY = mainCam.ScreenToWorldPoint(Input.mousePosition).y;
 			mousePos = new Vector3(mousePosX, mousePosY, 0);// select distance = 10 units from the camera
-			if (textCol.bounds.Contains(mousePos)) {
+			if (textCol.bounds.Contains(mousePos) && totalClicked < 3) {
+				totalClicked++;
 				rb.bodyType = RigidbodyType2D.Dynamic;
 				rb.AddForce(new Vector3(Random.Range(minForce.x, maxForce.x), Random.Range(minForce.y, maxForce.y), 0));
 				touched = true;
@@ -45,7 +51,12 @@ public class Text : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D other) {
 		if (touched && other.gameObject.tag == "Player") {
+			totalClicked--;
+			if (totalClicked < 0) {
+				totalClicked = 0;
+			}
 			transform.position = originalPos;
+			transform.rotation = originalRot;
 			rb.bodyType = RigidbodyType2D.Static;
 			touched = false;
 			Destroy(plane);
