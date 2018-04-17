b944cc668ffc7b5b713dca873e60d2de19b2f4fe
tday5 <trevday@icloud.com>
2018-04-17 10:35:45 -0400

    smoother camera

---
+++ New Unity Project/Assets/Scripts/OurCameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OurCameraFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start() {
+		gameObject.transform.position = player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		gameObject.transform.position = Vector2.Lerp(gameObject.transform.position, player.transform.position, Time.time);
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
+	}
+}
